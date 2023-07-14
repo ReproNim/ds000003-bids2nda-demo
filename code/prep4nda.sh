39e425906dd6bd079c8a7e99fcda973b1aac61aa
Yaroslav Halchenko <debian@onerussian.com>
2023-07-14 13:36:34 -0400

    now we want to get sub-03  into nda

--- code/prep4nda.sh
+++ code/prep4nda.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-git rm -rf sub-1* sub-0[3-9]  # remove subj I don't want to share
+git rm -rf sub-1* sub-0[4-9]  # remove subj I don't want to share
 git rm -r sub*/func  # remove functional I don't want to share
 git rm -r derivatives
